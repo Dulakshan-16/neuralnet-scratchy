36382b15c6c40656dc2436aca9a436e8bce070e3
Dulakshan <dulakshanperera@gmail.com>
2024-08-07 19:13:38 +0400

    Added relu activation function and neural network paper

--- visualizer/neural-net/neuralNetwork.js
+++ visualizer/neural-net/neuralNetwork.js
@@ -5,13 +5,20 @@ const math = create(all, config);
 
 const sigmoid = (z) => 1 / (1 + math.exp(-z));
 
+const relu = (z) => {
+  if (z <= 0) return 0;
+
+  return z;
+};
+
 class Neuron {
-  constructor(weights, x, bias, activation = "sig") {
+  constructor(weights, x, bias, activation = "sg") {
     this.w = weights;
     this.x = x;
     this.b = bias;
 
     if (activation === "sig") this.activation = sigmoid;
+    else if (activation === "relu") this.activation = relu;
   }
 
   compute_z() {
