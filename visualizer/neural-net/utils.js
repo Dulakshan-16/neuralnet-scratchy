8d1a0f93d7cf9ed397d016345fbcb5273e6bdbf2
Dulakshan <dulakshanperera@gmail.com>
2024-08-29 23:00:54 +0400

    Added comments that describe the structure of the weight matrix generated

--- visualizer/neural-net/utils.js
+++ visualizer/neural-net/utils.js
@@ -1,8 +1,12 @@
 export const randomWeight = () => Math.random() * 2 - 1;
 
 export const generateRandomWeightMatrix = (rows, columns) => {
+  // Row i corresponds to the weights that affect the ith neuron in the Layer
+  // Column j corresponds to the weights from the jth neuron from the previous layer that affect the neurons in the next layer
+  // W[i][j] corresponds to the weight that affects the ith neuron in the L layer from the jth neuron in L-1 layer
+
   let weightMatrix = new Array(rows);
-  
+
   for (let i = 0; i < rows; i++) {
     weightMatrix[i] = new Array(columns);
 
@@ -12,5 +16,5 @@ export const generateRandomWeightMatrix = (rows, columns) => {
     }
   }
 
-  return weightMatrix
+  return weightMatrix;
 };
