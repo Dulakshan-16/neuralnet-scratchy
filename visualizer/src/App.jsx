cff59b20922c3811330aafcaeb5dde3eac0e8591
Dulakshan <dulakshanperera@gmail.com>
2024-09-06 19:42:23 +0400

    simple text node which shows neuron data (Scuffed UI Neuron)

--- visualizer/src/App.jsx
+++ visualizer/src/App.jsx
@@ -8,8 +8,17 @@ function App() {
 
   return (
     <div>
-      <svg width="200" height="200" viewBox="0 0 200 200 " xmlns="http://www.w3.org/2000/svg">
-        <NeuronGraphic></NeuronGraphic>
+      <svg
+        width="500"
+        height="500"
+        viewBox="0 0 500 500 "
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <NeuronGraphic
+          w={[-1, 0.5]}
+          x={[1, 2]}
+          activation={"linear"}
+        ></NeuronGraphic>
       </svg>
     </div>
   );
--- visualizer/src/components/Neuron/NeuronGraphic.jsx
+++ visualizer/src/components/Neuron/NeuronGraphic.jsx
@@ -1,11 +1,17 @@
 import "./neuron.css";
-const NeuronGraphic = ({}) => {
+const NeuronGraphic = ({ w, x, activation }) => {
   return (
     <>
       <g id="neuron">
-        <circle cx="100" cy="100" r="50"></circle>
-        <text x="50%" y="50%" text-anchor="middle" dy=".3em">
-          test
+        <circle cx="250" cy="250" r="50"></circle>
+        <text x="50%" y="45%" text-anchor="middle" dy=".3em">
+          <tspan x="50%">input: {x}</tspan>
+          <tspan x="50%" dy="1.2em">
+            weights: {w.toString()}
+          </tspan>
+          <tspan x="50%" dy="1.2em">
+            activation: {activation}
+          </tspan>
         </text>
       </g>
     </>
